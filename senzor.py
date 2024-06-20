7c608ca229e5ac50144e59d4a66c2c5a6b04d84e
dmilic1 <148439139+dmilic1@users.noreply.github.com>
2024-06-20 15:35:00 +0200

    idk idc

---
+++ senzor.py
@@ -0,0 +1,17 @@
+from machine import ADC, Pin
+import time
+
+# Set up the ADC (Analog to Digital Converter) on pin GP26 (ADC0)
+soil_moisture_pin = ADC(Pin(26))
+
+# Function to read the soil moisture level
+def read_soil_moisture():
+    moisture_value = soil_moisture_pin.read_u16()  # Read the raw analog value (0-65535)
+    normalized = moisture_value/65535
+    return normalized
+
+# Main loop
+while True:
+    moisture_value = read_soil_moisture()
+    print("Soil Moisture Value:", moisture_value)
+    time.sleep(1)  # Delay for 1 second
